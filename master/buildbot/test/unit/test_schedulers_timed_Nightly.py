302dc1272cdd41f36f4842f2fe9640bbfacef55d
Chris AtLee <catlee@mozilla.com>
2010-08-20 17:24:17 -0400

    Move FakeDB connection into fakedb

--- master/buildbot/test/unit/test_schedulers_timed_Nightly.py
+++ master/buildbot/test/unit/test_schedulers_timed_Nightly.py
@@ -6,73 +6,7 @@ from twisted.internet import defer
 from buildbot.schedulers import timed
 from buildbot.changes.manager import ChangeManager
 from buildbot.changes.changes import Change
-
-class FakeDB:
-    def __init__(self):
-        self.schedulers = []
-        self.changes = []
-        self.sourcestamps = []
-        self.scheduler_states = {}
-        self.classified_changes = {}
-
-    def addSchedulers(self, schedulers):
-        i = len(self.schedulers)
-        for s in schedulers:
-            self.schedulers.append(s)
-            s.schedulerid = i
-            i += 1
-        return defer.succeed(True)
-
-    def addChangeToDatabase(self, change):
-        i = len(self.changes)
-        self.changes.append(change)
-        change.number = i
-
-    def get_sourcestampid(self, ss, t):
-        i = len(self.sourcestamps)
-        self.sourcestamps.append(ss)
-        ss.ssid = ss
-        return i
-
-    def runInteraction(self, f, *args):
-        return f(None, *args)
-
-    def scheduler_get_state(self, schedulerid, t):
-        return self.scheduler_states.get(schedulerid, {"last_processed": 0, "last_build": time.time()+100})
-
-    def scheduler_set_state(self, schedulerid, t, state):
-        self.scheduler_states[schedulerid] = state
-
-    def getLatestChangeNumberNow(self, t):
-        return len(self.changes)-1
-
-    def getChangesGreaterThan(self, last_changeid, t):
-        return self.changes[last_changeid:]
-
-    def scheduler_get_classified_changes(self, schedulerid, t):
-        return self.classified_changes.get(schedulerid, ([], []))
-
-    def scheduler_classify_change(self, schedulerid, changeid, important, t):
-        if schedulerid not in self.classified_changes:
-            self.classified_changes[schedulerid] = ([], [])
-
-        if important:
-            self.classified_changes[schedulerid][0].append(self.changes[changeid])
-        else:
-            self.classified_changes[schedulerid][1].append(self.changes[changeid])
-
-    def scheduler_retire_changes(self, schedulerid, changeids, t):
-        if schedulerid not in self.classified_changes:
-            return
-        for c in self.classified_changes[schedulerid][0][:]:
-            if c.number in changeids:
-                self.classified_changes[schedulerid][0].remove(c)
-        for c in self.classified_changes[schedulerid][1][:]:
-            if c.number in changeids:
-                self.classified_changes[schedulerid][1].remove(c)
-
-    def create_buildset(self, *args):
-        pass
+from buildbot.test.fake.fakedb import FakeDBConn
 
 class DummyParent:
     def __init__(self, dbconn):
@@ -83,10 +17,9 @@ class DummyParent:
     def publish_buildset(self, name, bsid, t):
         pass
 
-
 class Nightly(unittest.TestCase):
     def setUp(self):
-        self.dbc = FakeDB()
+        self.dbc = FakeDBConn()
 
     def test_dont_create_scheduler_changes(self):
         s = timed.Nightly(
